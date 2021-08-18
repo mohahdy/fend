4bdc66271e491421906a52aa8494fcb77ebb8a05
mohahdy <mohahdyc@hotmail.com>
2021-08-18 11:29:41 +0200

    First Trials
    first trials

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,41 +1,59 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-
+var sectionsList;
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
+function ReadSections ()
+{
+  return document.querySelectorAll('section');
+}
+
+function buildNavBar (secList)
+{
+  let navBarULElem = document.querySelector('#navbar__list');
+  console.log(navBarULElem);
+  if (secList !== null)
+  {
+    console.log(secList);
+    secList.forEach((item, i) => {
+  //    let li = document.createElement("li");
+    //  navBarULElem.append (li);
+      //li.append ("Section1   ");
+    });
 
 
-
+  }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
-
+console.log("TEST TST");
 // build the nav
-
-
+sectionsList = ReadSections();
+buildNavBar (sectionsList);
 // Add class 'active' to section when near top of viewport
 
 
@@ -45,13 +63,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
